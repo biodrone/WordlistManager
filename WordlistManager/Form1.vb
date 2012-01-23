3a2adcaaa877b8a01352113dd1d5515a21425610
Joshua Jacobs <josh.jacobs19@gmail.com>
2012-01-23 15:51:22 +0000

    0.0.2 - Compares but not properly. Going to use arrays now

--- WordlistManager/Form1.vb
+++ WordlistManager/Form1.vb
@@ -5,8 +5,6 @@ Public Class Form1
     Dim Wordlist As String = "", test As String, wrdPath As String = "", newFile As String = ""
     Dim DeDup As Boolean = False
     Private Sub Copy(sender As System.Object, e As System.EventArgs) Handles cmdCopy.Click
-        FileFind.CheckFileExists = True
-        FileFind.InitialDirectory = "%USERPROFILE%"
 
         If usrInputTxt.Text.Contains("\") Then
             If usrInputTxt.Text.Length > 1 Then
@@ -22,25 +20,42 @@ Public Class Form1
 
         newFile = Wordlist.Substring(0, Wordlist.LastIndexOf(".")) + "_NoDups.txt"
         If DeDup = True Then
-            Copy(newFile, True)
+            wrdmanage(newFile, True)
         Else
-            Copy(newFile, False)
+            wrdmanage(newFile, False)
         End If
-
 
     End Sub
 
     Public Function FileLoc(ByRef Location)
-
+        FileFind.CheckFileExists = True
+        FileFind.InitialDirectory = "%USERPROFILE%"
     End Function
 
-    Public Sub copy(ByVal newFile As String, ByVal DeDup As Boolean)
+    Public Sub wrdmanage(ByVal newFile As String, ByVal DeDup As Boolean)
         Dim sr As New StreamReader(Wordlist)
+        Dim Dup As New StreamReader(Wordlist)
+        Dim comWord As String = "", copWord As String = ""
 
-        While sr.EndOfStream = False
-            My.Computer.FileSystem.WriteAllText(newFile, sr.ReadLine + vbCrLf, True)
-        End While
+        If DeDup = True Then
+            While Dup.EndOfStream = False
+                comWord = Dup.ReadLine
+                While sr.EndOfStream = False
+                    copWord = sr.ReadLine
+                    If comWord <> copWord Then
+                        My.Computer.FileSystem.WriteAllText(newFile, copWord + vbCrLf, True)
+                    End If
+                End While
+                sr.Close()
+                sr = New StreamReader(Wordlist)
 
+            End While
+
+        Else
+            While sr.EndOfStream = False
+                My.Computer.FileSystem.WriteAllText(newFile, sr.ReadLine + vbCrLf, True)
+            End While
+        End If
         MsgBox("done!")
     End Sub
 
